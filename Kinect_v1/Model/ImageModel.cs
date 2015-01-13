94bd7be136c979bd0f85e451f377b2aca8f47b34
ynfin <yngfin@gmail.com>
2015-01-13 12:26:14 +0100

    Emgu referemces added
    Added references and libraries for EmguCV.

--- Kinect_v1/Model/ImageModel.cs
+++ Kinect_v1/Model/ImageModel.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
 
 namespace Kinect_v1.Model
 {
     class ImageModel
     {
+
+        public void setFrame()
+        {
+
+        }
+
+
+ 
+
     }
 }
--- Kinect_v1/dataCapture.cs
+++ Kinect_v1/dataCapture.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
 
 namespace Kinect_v1
 {
     class dataCapture
     {
+
+        private KinectSensor _sensor = null;
+        private ColorFrameReader _colorFrame = null;
+        private WriteableBitmap _colorBitmap = null;
+
+        public dataCapture()
+        {
+            // initialize kinect device
+        }
+
     }
 }
